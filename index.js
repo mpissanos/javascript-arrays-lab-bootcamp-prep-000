f79569aced0017c6a09b66fe5bed1b78e83fd77b
Manuel Pissanos <mpissano@gmail.com>
2018-08-21 19:30:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function destructivelyAppendKitten(name) {
 var kittens = ["Milo", "Otis", "Garfield"]
 
 
-function destructivelyAppendKitten(name) {
+function destructivelyprependKitten(name) {
   return kittens.unshift(name)
 }
